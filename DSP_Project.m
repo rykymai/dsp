8fb808d64dcbfaf82da4e704d3802715819ef06a
rykymai <rykymai@gmail.com>
2020-01-21 14:55:10 +0100

    .

--- DSP_Project.m
+++ DSP_Project.m
@@ -25,30 +25,50 @@ stem(n,x);                                          %Plot of the Input Signal
 %% 1 part
 X = dft(x, N);
 Y = dft(y, M);
-
+%pks=findpeaks(select,abs(X),'MinPeakHeight',1)
 %%Coherent Sampling Case
+%TODO :must be on the peaks of th e
 omega_X = 0:N-1*2*pi/N;
 %Spectral Plots 
 [mag_X, phase_X, mag_X_dB, phase_X_dB]=printGraph(X, omega_X, N);
-
+%close all;
 %%Non Coherent Sampling Case
 omega_Y = 0:M-1*2*pi/M;
 %Spectral Plots 
 [mag_Y, phase_Y, mag_Y_dB, phase_Y_dB]=printGraph(Y, omega_Y, M);
 
 %% 2 part
-SNR = 50;
+SNR = 50;         %TODO variare snr                                              %dB
 sz_x = size(x);
-sigman2 = 10^(SNR/20);
-noise_x = sqrt(sigman2).*randn(sz_x);                   % Random Noise
-
+%t_n= 0:1/fs:1/f0;
+%DONE : correzzione calcolo SNR
+%TODO cambiare plot  (DONE)
+%sigman2 = 10^(SNR/20);
 A1 = (A / 100) * 10;
+sigman2 =    A^2*0.5*10^(-SNR/10)*300;  %A^2/
+variance = 10^(-SNR/10);
+
+noise_x = sqrt(sigman2).*randn(sz_x); 
+figure
+plot(noise_x)
+% Random Noise
+%noise_x = awgn(x,SNR,'measured','db');
+a=x+ noise_x;
+figure;
+plot(x);
+hold on;
+plot(noise_x);
+hold on;
+plot(a);
+
 HarmI_X = A1 * sin(2*pi*2*f0/fs*n + phi);               % Harmonic II
 HarmII_X = A1 * sin(2*pi*3*f0/fs*n + phi);              % Harmonic III   
 sig_corrupted_X = x + noise_x + HarmI_X + HarmII_X;     % Output Signal
 total_noise = noise_x + HarmI_X + HarmII_X;             % Total Amount of Noise
 
 % Plots
+figure;
+plot(noise_x);
 figure;
 plot(total_noise);
 title('Total noise');
@@ -85,10 +105,10 @@ mean_harm2 = mean(HarmI_X);
 variance_harm2 = var(HarmI_X);
 mean_harm3 = mean(HarmII_X);
 variance_harm3 = var(HarmII_X);
-
+%close all;
 %% Part 3
-P = 10;
-for i=1:1:P
+P = 10;                                        % Parameter Beta to vary
+for i=1:1:P                          
     wind_k = kaiser(L, i);                      % Kaiser Window
     x_wind_n = x.*wind_k';                      % Applying Kaiser Window
     figure(49);
@@ -135,15 +155,18 @@ for i=1:1:P
 end
 
 sz_x = size(x);
-sigman2 = 10^(SNR/20);
-noise_x = sqrt(sigman2)*randn(sz_x);
+%sigman2 = 10^(-SNR/20);
+%TODO cambiare plot
+sigman2 =  A^2*0.5*10^(-SNR/10);
 A1 = (A / 100) * 10;
+
+noise_x = sqrt(sigman2).*randn(sz_x);
 figure(53);
 subplot(5,1,2);
 plot(n, noise_x);
 xlim([0 L]);
 ylabel('Random Noise');
-
+%close all;
 %% Part 4
 P = 10;
 for i=1:1:P
@@ -151,8 +174,9 @@ for i=1:1:P
     x_wind_n = x.*x_wind_noise';                                            % Applying Kaiser Window
     HarmI_X_win = A1 * sin(2*pi*2*f0/fs*n + phi).*x_wind_noise';
     HarmII_X_win = A1 * sin(2*pi*3*f0/fs*n + phi).*x_wind_noise';
+    %noise_x=0;
     sig_corrupted_X_win = x_wind_n + noise_x + HarmI_X_win + HarmII_X_win;
-    
+    %TODO change report cap 7
     % Plots
     figure(53);
     subplot(5,1,1);
--- printGraph.m
+++ printGraph.m
@@ -11,8 +11,11 @@ figure('Name', 'MAG[ABS(X)]');
 plot(omega, magf);
 xlim([omega(1) omega(end)]);
 %stem(omega, mag);
-
+%[pks,loc]=findpeaks(magf);
+% pks
+% loc
 phi=20*log10(angle(X));
+
 phif = angle(X);
 figure('Name', 'angle ABS(X)');
 subplot(2,1,1);
@@ -28,8 +31,10 @@ xlim([omega(1) omega(end)]);
 %stem(omega, phi);
 ylabel('Phase');
 xlabel('Bins');
-
-mag =  abs(X(N/2));
+ 
+%mag =  abs(X(N/2));
+mag1 =  abs(X(N/2));
+mag =  abs(X(2));
 phi = angle(X(N/2));
 
 mag_dB =  20*log10(abs(X(N/2)));
